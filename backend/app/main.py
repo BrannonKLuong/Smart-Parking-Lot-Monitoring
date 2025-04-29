207cea7ccf77121f3bf11d2d30b540bf6a1174bf
BrannonKLuong <brannon.k.luong@gmail.com>
2025-04-29 13:10:59 -0700

    Added a way to stream a pre recorded video onto the website

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import StreamingResponse
 from inference.model import detect
+from app.video import make_frames
 import numpy as np
 import cv2
 
@@ -20,3 +22,34 @@ async def detect_route(file: UploadFile = File(...)):
             cls = int(box.cls[0])
             detections.append({"bbox": [x1, y1, x2, y2], "conf": conf, "class": cls})
     return {"count": len(detections), "detections": detections}
+
+@app.get('/video_feed')
+async def video_feed():
+    async def streamer():
+        for frame in make_frames('sample_videos/sample.mp4'):
+            results = detect(frame)
+            for r in results:
+                for box in r.boxes:
+                    x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame_bytes = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+    return StreamingResponse(streamer(), media_type='multipart/x-mixed-replace; boundary=frame')
+
+@app.get('/webcam_feed')
+
+async def webcam_feed():
+    async def gen():
+        for frame in make_frames(0):
+            results = detect(frame)
+            for r in results:
+                for box in r.boxes:
+                    x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame_bytes = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+    return StreamingResponse(gen(), media_type='multipart/x-mixed-replace; boundary=frame')
