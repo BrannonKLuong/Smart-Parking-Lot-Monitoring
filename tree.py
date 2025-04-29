ce8fdca03aad1c5ed629c012eb26fb4b5a311c1c
BrannonKLuong <brannon.k.luong@gmail.com>
2025-04-29 16:39:06 -0700

    Added a python get Tree of folder hiearchy to illustrate current structure

---
+++ tree.py
@@ -0,0 +1,21 @@
+import os
+
+
+def print_tree(path, prefix=''):
+    entries = sorted(os.listdir(path))
+    for idx, name in enumerate(entries):
+        # 1) Skip .venv
+        if name == '.venv' or name == ".git":
+            continue
+
+        full = os.path.join(path, name)
+        connector = '└── ' if idx == len(entries)-1 else '├── '
+        print(prefix + connector + name)
+        if os.path.isdir(full):
+            extension = '    ' if idx == len(entries)-1 else '│   '
+            print_tree(full, prefix + extension)
+
+if __name__ == '__main__':
+    root = r'C:\Users\Brannon Luong\Desktop\Smart Parking-Lot Monitoring'
+    print(root)
+    print_tree(root)
