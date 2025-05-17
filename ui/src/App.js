c99e08c4ac8366d152f294b2b0d028e76d348fea
BrannonKLuong <brannon.k.luong@gmail.com>
2025-05-16 21:08:07 -0700

    Test frontend CI/CD

--- ui/src/App.js
+++ ui/src/App.js
@@ -4,7 +4,7 @@ import Notifications from './Notifications';
 import SpotTile      from './SpotTile';
 import SpotsEditor   from './SpotsEditor';
 import './index.css';
-
+// Test Push
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
 const WS_BASE_URL  = process.env.REACT_APP_WS_BASE_URL || 'ws://localhost:8000';
 
